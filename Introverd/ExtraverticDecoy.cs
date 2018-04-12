ae66b9ef0ca82eed3a62f0fc980b64d04f0f5377
MiraiXD <37700015+MiraiXD@users.noreply.github.com>
2018-04-12 23:12:28 +0200

    Introverd

---
+++ Introverd/ExtraverticDecoy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraverticDecoy : AbilityButton
+{
+
+    public float _rechargeTime = 5;
+
+    // Use this for initialization
+    public void Start()
+    {
+        base.Start();
+        SetRechargeTime(_rechargeTime);
+        
+    }
+
+    public override void Ability()
+    {
+        print("EXTRAVERTICDECOY");
+    }
+}
---
+++ Introverd/Player.cs
@@ -0,0 +1,48 @@
+﻿// This script manages scenes and gameover
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class Player : MonoBehaviour {
+
+    // You lose the game when you reach maxNumberOfFriends
+    [SerializeField]
+    public int maxNumberOfFriends = 5;
+    // Current number of friends
+    int friends = 0;
+    //Time to wait after the game has been lost
+    int timeToQuit = 5;
+    // Invoked when an enemy has gone through your defences
+    public void NewFriend()
+    {
+        friends++;
+        if (friends >= maxNumberOfFriends) EndGame();
+    }
+
+    void EndGame()
+    {
+        GameObject gameOver = GameObject.FindWithTag("GameOver");
+        gameOver.GetComponent<Image>().enabled = true;
+        gameOver.GetComponentInChildren<Text>().enabled = true;
+        StartCoroutine(WaitAndQuit(timeToQuit));
+    }
+
+    IEnumerator WaitAndQuit(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        // Load the menu scene
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MenuScene");
+        }
+    }
+
+}
